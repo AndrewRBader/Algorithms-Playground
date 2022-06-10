db26199f6cbffed573e8c2365688155df1794544
RyanMacc <arbader2142@gmail.com>
2022-06-10 10:15:16 -0400

    jquery styles up and running! here we go! :)

--- public/scripts/arrays_strings/duplicate_check.js
+++ public/scripts/arrays_strings/duplicate_check.js
@@ -1,9 +1,20 @@
 const $visualizerDiv = $('<div>Algorithm Visualizer Activated</div>')
 
 
+
 $('#visualizer-button').click(() => {
     $visualizerDiv.appendTo('.visualizer-span')
+
+    var visDivStyle = {
+        "background-color":"blue",
+        "border-style":"solid"
+    }
+
+    return function () {
+        $visualizerDiv.css(visDivStyle)
+    }()
 })
+
 
 $('#reset-button').click(() => {
     $visualizerDiv.remove();
